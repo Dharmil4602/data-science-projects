26d000f6553f924109e59e03755e886a0b8688fa
Dharmil Shah <dharmilshah476@gmail.com>
2022-09-12 01:30:31 +0530

    Updated the dataset and added map

--- Motor Vehicle Collision Crashes/app.py
+++ Motor Vehicle Collision Crashes/app.py
@@ -5,14 +5,24 @@ st.markdown("This application is a Streamlit dashboard that can be used to analy
 
 @st.cache(persist=True)
 def load_data(nrows):
-    data = pd.read_csv('data.csv', nrows=nrows, parse_dates=[['CRASH DATE', 'CRASH TIME']])
+    data = pd.read_csv('data.csv', nrows=nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
     data.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
-    data.rename(columns={'crash date_crash time': 'date/time'}, inplace=True)
+    data.rename(columns={'crash_date_crash_time': 'date/time'}, inplace=True)
     return data
 
 data = load_data(100000)
+st.header("Where are the most injured people in NYC?")
+injured_people = st.slider("Number of persons injured in vehicle collisions", 0, 19)
+st.map(data.query('injured_persons >= @injured_people')[['latitude', 'longitude']].dropna(how="any"))
+
+
+
+
+
+
+
 if st.checkbox("Show Raw Data", False):
     st.subheader("Raw data")
     st.write(data)
