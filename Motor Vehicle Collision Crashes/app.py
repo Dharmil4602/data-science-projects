1aa497a9402afe2b1765e6178c514acc507d14b6
Dharmil Shah <dharmilshah476@gmail.com>
2022-09-12 00:20:41 +0530

    Read the data

--- Motor Vehicle Collision Crashes/app.py
+++ Motor Vehicle Collision Crashes/app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 import pandas as pd
-st.title("Hello There")
-st.markdown("This is a markdown")
+DATA_URL = pd.read_csv('data.csv')
+st.title("Motor Vehicle Collisions in New York City")
+st.markdown("This application is a Streamlit dashboard that can be used to analyze motor vehicle collisions in NYC 🗽💥🚗")
